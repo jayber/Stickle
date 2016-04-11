69e367ebdb5482a264a43dbe03daf3a0028d530a
James <james.a.bromley@googlemail.com>
2016-04-11 23:07:46 +0100

    use ionic

--- www/js/index.js
+++ www/js/index.js
@@ -1,18 +1,14 @@
-var app = {
 
-    initialize: function () {
-        this.bindEvents();
-    },
+angular.module('stickle', ['ionic'])
 
-    bindEvents: function () {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
+    .controller('stickleCtrl', function($scope) {
+        $scope.contacts = [];
+        contactProcessor.populateContacts($scope);
+    });
 
-    onDeviceReady: function () {
-        $.Deferred(app.populateContacts());
-    },
+var contactProcessor = {
 
-    populateContacts: function () {
+    populateContacts: function (model) {
         var fields = [''];
         var options = new ContactFindOptions();
         options.filter="";
@@ -21,22 +17,36 @@ var app = {
             navigator.contacts.fieldType.name,
             navigator.contacts.fieldType.phoneNumbers];
         navigator.contacts.find(fields,
-            function (contacts) {
-                var cleanContacts = app.filterOnPhoneAndSortByNameAlphabet(contacts);
-                cleanContacts.forEach(function (contact) {
-                    $.Deferred(app.processContact(contact));
-                })
-            }, function(contactError) {
-            context.alert('error finding contacts');
+            function(contacts) {contactProcessor.processContacts(contacts,model)},
+            function(contactError) {
+                context.print('error finding contacts');
         }, options);
     },
 
-    makeCall: function () {
-        window.plugins.CallNumber.callNumber(function () {
-            alert('success');
-        }, function () {
-            alert('error');
-        }, 0);
+    processContacts: function (contacts, model) {
+        var cleanContacts = contactProcessor.filterOnPhoneAndSortByNameAlphabet(contacts);
+        cleanContacts.forEach(function (contact) {
+            $.Deferred(function() {contactProcessor.processContact(contact, model)});
+        });
+    },
+
+    filterOnPhoneAndSortByNameAlphabet: function (contacts) {
+        return contacts.filter(function (contact) {
+            return contact.phoneNumbers != null
+        }).
+            sort(function (a, b) {
+                if (a.displayName < b.displayName)
+                    return -1;
+                else if (a.displayName > b.displayName)
+                    return 1;
+                else
+                    return 0;
+            });
+    },
+
+    processContact: function (contact, model) {
+        contact = contactProcessor.dedupePhoneNumbers(contact);
+        contactProcessor.storeAndDisplayIfNew(contact, model);
     },
 
     dedupePhoneNumbers: function (contact) {
@@ -54,23 +64,19 @@ var app = {
         return contact;
     },
 
-    processContact: function (contact) {
-        contact = this.dedupePhoneNumbers(contact);
-        context.storeAndDisplayIfNew(contact);
+    storeAndDisplayIfNew: function(contact, model) {
+        model.$apply(function(){
+            model.contacts.push(contact);
+        });
+        context.hideContactsLoading();
     },
 
-    filterOnPhoneAndSortByNameAlphabet: function (contacts) {
-        return contacts.filter(function (contact) {
-            return contact.phoneNumbers != null
-        }).
-            sort(function (a, b) {
-                if (a.displayName < b.displayName)
-                    return -1;
-                else if (a.displayName > b.displayName)
-                    return 1;
-                else
-                    return 0;
-            });
+    makeCall: function () {
+        window.plugins.CallNumber.callNumber(function () {
+            alert('success');
+        }, function () {
+            alert('error');
+        }, 0);
     }
 };
 
@@ -78,32 +84,12 @@ var context = {
     contactsElement: $('#contacts'),
     errorsElement: $('#errors'),
 
-    alert: function(error) {
+    print: function(error) {
         context.errorsElement.show();
         context.errorsElement.append(error);
     },
 
-    storeAndDisplayIfNew: function(contact) {
-        context.hideContactsLoading();
-        var contactHtml = context.render('contactTemplate', contact);
-        context.contactsElement.append(contactHtml);
-    },
-
     hideContactsLoading: function() {
         $('.loading').hide();
-    },
-
-    render: function (tmpl_name, tmpl_data) {
-        if (!context.tmpl_cache) {
-            context.tmpl_cache = {};
-        }
-
-        if (!context.tmpl_cache[tmpl_name]) {
-            var tmpl_string = document.getElementById(tmpl_name).innerHTML;
-
-            context.tmpl_cache[tmpl_name] = Handlebars.compile(tmpl_string);
-        }
-
-        return context.tmpl_cache[tmpl_name](tmpl_data);
     }
 };
