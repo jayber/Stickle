ca3be9bea02495790debcad6a43b381cd77f3ca3
James <james.a.bromley@googlemail.com>
2016-07-12 10:02:47 +0100

    rollback some log changes to work on compiled mobile app

--- www/js/context.js
+++ www/js/context.js
@@ -37,7 +37,7 @@ var context = {
             if (stack) {
                 message += '\n' + stack;
             }
-            log.error(message);
+            alert(message);
         });
     },
 
