f531ce6f69fa317aa1f01fa9b7c239682a8fe456
James <james.a.bromley@googlemail.com>
2016-08-18 16:27:34 +0100

    to work on dogee

--- www/js/telephone.js
+++ www/js/telephone.js
@@ -1,19 +1,22 @@
 //has tests
 var telephone = {
+    startsWith: function(str, target) {
+      return str.indexOf(target) == 0;
+    },
     canonicalize: function(telNo) {
         log.trace("canonicalizing");
         try {
         var result = telNo.replace(new RegExp("\\D*","g"),'');
 
-        if (result.startsWith('00')) {
+        if (telephone.startsWith(result,'00')) {
             result = result.substr(2,result.length);
         }
 
-        if (result.startsWith('44') || result.startsWith('0') || result.length <= 4) {
-            if (result.startsWith('44')) {
+        if (telephone.startsWith(result, '44') || telephone.startsWith(result, '0') || result.length <= 4) {
+            if (telephone.startsWith(result, '44')) {
                 result = result.substr(2,result.length);
             }
-            if (result.startsWith('0')) {
+            if (telephone.startsWith(result, '0')) {
                 result = result.substr(1, result.length);
             }
             result = "44".concat(result);
