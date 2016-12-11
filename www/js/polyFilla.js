5b12f8236d592a61b5a38e9958d6200bf4573c1d
James <james.a.bromley@googlemail.com>
2016-12-11 15:08:19 +0000

    remove unnecessary permissions

--- www/js/polyFilla.js
+++ www/js/polyFilla.js
@@ -14,10 +14,38 @@ function polyFillMobileAPIs() {
                     stickler: false,
                     phoneNumbers: [{type: "mobile", value: "07797814997"}]
                 }, {
+                    displayName: "Alba Veronica",
+                    stickler: false,
+                    phoneNumbers: [{type: "mobile", value: "07678006123"}]
+                }, {
+                    displayName: "Canada Goose",
+                    stickler: false,
+                    phoneNumbers: [{type: "mobile", value: "07799887123"}]
+                }, {
+                    displayName: "Hilary Campion",
+                    stickler: false,
+                    phoneNumbers: [{type: "mobile", value: "07777895546"}]
+                }, {
+                    displayName: "Roseanne Hillmore",
+                    stickler: false,
+                    phoneNumbers: [{type: "mobile", value: "07891178261"}]
+                }, {
+                    displayName: "Charlie Bright",
+                    stickler: false,
+                    phoneNumbers: [{type: "mobile", value: "07712996181"}]
+                }, {
+                    displayName: "Octavia Ballantine",
+                    stickler: false,
+                    phoneNumbers: [{type: "mobile", value: "07797814997"}]
+                }, {
+                    displayName: "Vladimir Kundalini",
+                    stickler: false,
+                    phoneNumbers: [{type: "mobile", value: "07789087614"}]
+                }, {
                     displayName: "Clay Easton",
                     stickler: true,
                     phoneNumbers: [{type: "mobile", value: "08991872567"}, {type: "home", value: "02035678906"}]
-                }, {
+                } ,{
                     displayName: "East Bay Ray",
                     stickler: true,
                     phoneNumbers: [{type: "mobile", value: "2222"}]
